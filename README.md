7ff5e3563767ab74c23999ecf975dfe5b97592ff
Harsh <imlucifer81@gmail.com>
2026-05-06 12:02:18 +0000

    Add README content and computer choice code in JS

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rock Paper Scissor
+
+A game called rock paper scissors played bitween user and computer as part of The Odin Project curriculum.
+
+## Built With 
+- HTML
+- Javascript
---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return "Rock";
+    }   else if (choice === 1) {
+        return "Paper";
+    }   else if (choice === 2) {
+        return "Scissor";
+    }
+}
+
+console.log(getComputerChoice())
